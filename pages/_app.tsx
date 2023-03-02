451984dabbd8871ebd6db2a3891291e8d286a10c
0length <fajarjunyor13@gmail.com>
2023-03-02 12:55:24 +0700

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Router from 'next/router';
 import nProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 Router.events.on('routeChangeStart', () => nProgress.start());
 Router.events.on('routeChangeComplete', () => nProgress.done());
