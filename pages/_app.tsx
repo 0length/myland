e31d83aa2304cdc03c87062aa918e6c53ea3536b
0length <fajarjunyor13@gmail.com>
2023-03-02 12:47:29 +0700

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Router from 'next/router';
+import nProgress from 'nprogress';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
+// small change
+nProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
